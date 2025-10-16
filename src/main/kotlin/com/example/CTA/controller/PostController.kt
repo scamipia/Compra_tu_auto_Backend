52965ff48f6447b6bf4337fa30bad6de9b5f6a4a
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2025-10-16 15:31:51 -0300

    test de controladores y cambio de ruta en search

--- src/main/kotlin/com/example/CTA/controller/PostController.kt
+++ src/main/kotlin/com/example/CTA/controller/PostController.kt
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/post")
 class PostController {
 
     @Autowired
@@ -39,7 +41,7 @@ class PostController {
             }
     }
 
-    @GetMapping("/post/{id}")
+    @GetMapping("/{id}")
     fun getPost(@PathVariable id: Long): PostResponseDTO {
         val post = postService.getPostById(id)
         return postToDTO(post)
