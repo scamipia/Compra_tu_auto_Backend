a68f76077e80a33ad92d20813968250be00f73c0
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2025-11-13 21:54:52 -0300

    fix error h2 columna year

--- src/main/kotlin/com/example/CTA/model/Car.kt
+++ src/main/kotlin/com/example/CTA/model/Car.kt
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "car_table")
 class Car {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="car_id")
     var id:Long? = null
 
     @Column(nullable = false)
@@ -34,7 +37,7 @@ class Car {
     @Column
     var horsepower: Int? = null
 
-    @Column
+    @Column(name = "manufacture_year")
     var year: Int? = null
 
     @Column
